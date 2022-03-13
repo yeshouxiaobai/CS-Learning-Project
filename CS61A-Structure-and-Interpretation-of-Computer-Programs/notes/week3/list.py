79439cb73cbfdf6c55e549dd2ee4a7ba6046bf68
zhuguiyuan <951620305@qq.com>
2022-03-13 22:12:14 +0800

    Update CS61A

---
+++ CS61A-Structure-and-Interpretation-of-Computer-Programs/notes/week3/list.py
@@ -0,0 +1,18 @@
+from operator import getitem, add, mul
+
+
+digits = [1, 8, 2, 8]
+
+getitem(digits, 0)
+
+add(digits, mul(digits, 2))
+
+1 in digits
+
+'1' in digits
+
+[1, 8] not in digits
+
+[1, 2] in [3, [1, 2], 4]
+
+[1, 2] in [3, [[1, 2]], 4]
